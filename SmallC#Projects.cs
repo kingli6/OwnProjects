d54b4e2874b693ee36fea131f61b76ed5f7c2d24
Jonathan Fisher <jonathan.fshr@gmail.com>
2021-08-18 15:26:37 +0200

    methods training

--- SmallC#Projects.cs
+++ SmallC#Projects.cs
@@ -1,5 +1,61 @@
 
 /*
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Program p  = new Program();
+		Console.WriteLine(p.Paragraph("World"));
+	}
+	////////////////////////////////
+	 //function enum months
+	public int Months(enum.month){
+		enum Month {Januari = 31, Februari = 30, Mars = 31, April = 30, Maj = 31, Juni = 30,
+					Juli = 31, Augusti = 30, September = 31, October = 30, November = 31, December = 30,}
+		return 
+	}
+	
+	//function 6 returnera "<p>string</p>"
+	public string Paragraph(string a){
+		return("<p>"+a+"</p>");
+	}
+	
+	//function 5 avrunda ett tal till två decimaler
+	public double Avrunda(double num){
+		return Math.Round(num,2);
+		//int placeHolder = num;
+		//double placeHolder2 = placeHolder - num;
+		//example
+		//3.45	3 and 0.45 
+		//
+	}
+	
+	//function 4 Välkommen
+	public void Välkommen(string name, string stad, ushort num){
+		//Console.WriteLine($"Välkommen {name} från {stad} med favorittal {num}");
+		Console.WriteLine("Välkommen {0} från {1} med favorittal {2}", name, stad, num);
+	}
+	
+	//fucntion 3 using multi and add
+	public double Func3(double a, double b, double c, double d){
+		//double x = Multi(a, b, c);
+		//return(Add(x,d));
+		return(Add(Multi(a, b, c), d));
+	}
+	
+	//function 2 multi STATIC
+	public static double Multi(double a, double b, double c){
+		return a * b * c;
+	}
+	
+	//Function 1 Add
+	public double Add(double a, double b){
+		return a+b;
+	}
+	
+}
 *
 public static void Main()
 	{
